ee4750630181d2ad8c16eb965b05c24b65c095ec
Omer Kose <omerkose@Omer-MacBook-Air.local>
2023-06-03 16:27:24 +0300

    isLogin condition added to Router

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -4,18 +4,34 @@ import HomeScreen from "./HomeScreen";
 import ProfileScreen from "./ProfileScreen";
 import {NavigationContainer} from "@react-navigation/native";
 import * as React from "react";
+import {useContext} from "react";
+import {LoginContext} from "../contexts/LoginContext";
 
 export default function Router() {
 
+    let { isLogin } = useContext(LoginContext)
+
     const Stack = createNativeStackNavigator();
+
+    function HomeTabs() {
+        return(
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Profile" component={ProfileScreen} />
+            </Stack.Navigator>
+        )
+    }
 
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator>
 
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Profile" component={ProfileScreen} />
+                {
+                    isLogin ?
+                        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+                        :
+                        <Stack.Screen name="Login" component={LoginScreen} />
+                }
 
             </Stack.Navigator>
         </NavigationContainer>
